9eb662fd5199cddd5a20fe8e0048770d289dbfb7
Victor van Wymeersch <Victor.vanwymeersch@gmail.com>
2020-12-11 14:51:47 +0100

    Basic splash screen implemented without functionality

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,4 +1,5 @@
 // Import packages:
+import java.awt.*;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -48,73 +49,75 @@ public class GameManager {
         Player player1 = new Player("victor", false);
         Player player2 = new Player("ines", false);
 
-        // Play the game:
-        boolean gameRunning = true;
-        // TODO set player turn from GUI
-        boolean player1Turn = true;
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("\nGame has begun ... \n");
-        while (gameRunning) {
-            System.out.println("Current Score:");
-            System.out.println(player1.getName()+" = "+ String.valueOf(player1.getScore()));
-            System.out.println(player2.getName()+" = "+ String.valueOf(player2.getScore())+"\n");
-            if (player1Turn == false) {  // Player 2 turn
-                System.out.println(player2.getName()+"'s turn! Take your shot.\n");
-//                System.out.println(player1.getName() + "'s board:");
-                System.out.println(boardPlayer1.showBoard());
-            }else {  // Player 1 turn
-                System.out.println(player1.getName()+"'s Turn! Take your shot...\n");
-//                System.out.println(player2.getName() + "'s board:");
-                System.out.println(boardPlayer2.showBoard());
-            }
+//        // Play the game:
+//        boolean gameRunning = true;
+//        // TODO set player turn from GUI
+//        boolean player1Turn = true;
+//        Scanner userInput = new Scanner(System.in);
+//        System.out.println("\nGame has begun ... \n");
+//        while (gameRunning) {
+//            System.out.println("Current Score:");
+//            System.out.println(player1.getName()+" = "+ String.valueOf(player1.getScore()));
+//            System.out.println(player2.getName()+" = "+ String.valueOf(player2.getScore())+"\n");
+//            if (player1Turn == false) {  // Player 2 turn
+//                System.out.println(player2.getName()+"'s turn! Take your shot.\n");
+//                System.out.println(boardPlayer1.showBoard());
+//            }else {  // Player 1 turn
+//                System.out.println(player1.getName()+"'s Turn! Take your shot...\n");
+//                System.out.println(boardPlayer2.showBoard());
+//            }
+//
+//            // Take shot at board:
+//            // Get user input for attack coordinates
+//            System.out.println("Enter attack coordinates (x=1..8, y=1..8) {without brackets and separated with comma}");
+//            String input = userInput.nextLine();
+//            int x = Integer.parseInt(input.split(",")[0]);
+//            int y = Integer.parseInt(input.split(",")[1]);
+//            int[] attackCoordinates = new int[] {x-1,y-1};
+//            if (player1Turn == false) {  // Player 2 turn
+//                // See if player gets points for shot
+//                int pointsForHit = boardPlayer1.pointsForHit(attackCoordinates);
+//                if ( pointsForHit > 0){
+//                    System.out.println("Hit!");
+//                    // Assign points for hit
+//                    player2.increaseScore(pointsForHit);
+//                    // Check if game is over
+//                    if (boardPlayer1.areAllShipsSunk()){
+//                        gameRunning = false;
+//                    }
+//                }else{
+//                    System.out.println("Miss!");
+//                }
+//            }else{  // Player 1 turn
+//                int pointsForHit = boardPlayer2.pointsForHit(attackCoordinates);
+//                if ( pointsForHit > 0){
+//                    System.out.println("Hit!");
+//                    // Assign points for hit
+//                    player1.increaseScore(pointsForHit);
+//                    // Check if game is over
+//                    if (boardPlayer2.areAllShipsSunk()){
+//                        gameRunning = false;
+//                    }
+//                }else{
+//                    System.out.println("Miss!");
+//                }
+//            }
+//        // Next players turn
+//        player1Turn = !player1Turn;
+//
+//        } // end of game loop
+//
+//        if (player1.getScore()>player2.getScore()){
+//            System.out.println(player1.getName()+" has won the game!");
+//        }else if (player1.getScore()<player2.getScore()){
+//            System.out.println(player2.getName()+" has won the game!");
+//        }else{
+//            System.out.println("The game is a tie!");
+//        }
 
-            // Take shot at board:
-            // Get user input for attack coordinates
-            System.out.println("Enter attack coordinates (x=1..8, y=1..8) {without brackets and separated with comma}");
-            String input = userInput.nextLine();
-            int x = Integer.parseInt(input.split(",")[0]);
-            int y = Integer.parseInt(input.split(",")[1]);
-            int[] attackCoordinates = new int[] {x-1,y-1};
-            if (player1Turn == false) {  // Player 2 turn
-                // See if player gets points for shot
-                int pointsForHit = boardPlayer1.pointsForHit(attackCoordinates);
-                if ( pointsForHit > 0){
-                    System.out.println("Hit!");
-                    // Assign points for hit
-                    player2.increaseScore(pointsForHit);
-                    // Check if game is over
-                    if (boardPlayer1.areAllShipsSunk()){
-                        gameRunning = false;
-                    }
-                }else{
-                    System.out.println("Miss!");
-                }
-            }else{  // Player 1 turn
-                int pointsForHit = boardPlayer2.pointsForHit(attackCoordinates);
-                if ( pointsForHit > 0){
-                    System.out.println("Hit!");
-                    // Assign points for hit
-                    player1.increaseScore(pointsForHit);
-                    // Check if game is over
-                    if (boardPlayer2.areAllShipsSunk()){
-                        gameRunning = false;
-                    }
-                }else{
-                    System.out.println("Miss!");
-                }
-            }
-        // Next players turn
-        player1Turn = !player1Turn;
+        // GUI
+        MenuManager gui = new MenuManager();
 
-        } // end of game loop
-
-        if (player1.getScore()>player2.getScore()){
-            System.out.println(player1.getName()+" has won the game!");
-        }else if (player1.getScore()<player2.getScore()){
-            System.out.println(player2.getName()+" has won the game!");
-        }else{
-            System.out.println("The game is a tie!");
-        }
     }
 
     private static int[] readBoardSize(String filename) {
@@ -149,6 +152,10 @@ public class GameManager {
         }
         int[] boardSize = new int[] {boardDimension, boardDimension};
         return boardSize;
+    }
+
+    private static void createGUI(){
+
     }
 
 }
